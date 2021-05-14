aff67c7cef71153dc6a312806c11c9f2f0963b86
Dmitry Selikhov <selikhov.dmitrey@gmail.com>
2021-05-13 23:56:38 -0400

    Lookup currency codes based on ISO 4217

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const combine = (code) => {
 const getKeyByValue = (object, value) => Object.keys(object).find(key => object[key] === value)
 
 module.exports = {
-    code: (code) => combine(data.find(c => c.code === code.toUpperCase())),
-    symbol: (symbol) => combine(data.find(c => c.code === getKeyByValue(symbols, symbol))),
-    country: (country) => data.filter(c => (c.countries.map((c) => c.toLowerCase()) || []).indexOf(country.toLowerCase()) > -1),
-    number: (number) => data.find((c) => c.number === String(number)),
-    codes: () => data.map((c) => c.code),
-    numbers: () => data.map((c) => c.number).filter(n => !!n),
-    countries: () => data.map((c) => c.countries).flat().filter((c, i, arr) => arr.indexOf(c) === i),
+    code: code => combine(data.find(c => c.code === code.toUpperCase())),
+    symbol: symbol => combine(data.find(c => c.code === getKeyByValue(symbols, symbol))),
+    country: country => data.filter(c => (c.countries.map((c) => c.toLowerCase()) || []).indexOf(country.toLowerCase()) > -1),
+    number: number => data.find(c => c.number === String(number)),
+    codes: () => data.map(c => c.code),
+    numbers: () => data.map(c => c.number).filter(n => !!n),
+    countries: () => data.map(c => c.countries).flat().filter((c, i, arr) => arr.indexOf(c) === i),
     data,
     symbols,
     publishDate: require('./iso-4217-publish-date')
